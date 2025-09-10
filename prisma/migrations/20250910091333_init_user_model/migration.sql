204f438762c620b2898a92c4556213d2968149ca
EnguerranCA <enguerran.caroalquier@etu.unilim.fr>
2025-09-10 11:19:00 +0200

    Migration

---
+++ prisma/migrations/20250910091333_init_user_model/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "pseudo" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_pseudo_key" ON "public"."User"("pseudo");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
